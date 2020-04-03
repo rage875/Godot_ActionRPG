124189812f7624b6884e1f03c80ef6d8fd1609ef
Luis03 <rage.875@gmail.com>
2020-04-03 16:36:45 -0600

    Animation tree mechanism added to Player scene movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,9 +5,18 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+# velocity global variable
 var velocity = Vector2.ZERO
 
-# Ready callback
+# Variables will be set when ready function
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var paramtersPlayback = "parameters/playback"
+onready var parametersIdle = "parameters/Idle/blend_position"
+onready var parametersRun = "parameters/Run/blend_position"
+onready var animationState = animationTree.get(paramtersPlayback)
+
+# Ready callbackS
 func _ready():
 	print("Player body is ready")
 
@@ -19,8 +28,13 @@ func _physics_process(delta):
 	inputVector = inputVector.normalized()
 	
 	if Vector2.ZERO != inputVector:
+		animationTree.set(parametersIdle, inputVector)
+		animationTree.set(parametersRun, inputVector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
+		animationTree.set(parametersRun, inputVector)
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
